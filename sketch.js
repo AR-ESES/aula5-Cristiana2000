a5fce81bb9dddb4fa1e6162a23b4f39c60ab4101
Cristiana2000 <102221965+Cristiana2000@users.noreply.github.com>
2022-03-30 10:46:39 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,10 +8,26 @@ function setup() {
 
 function draw() {
 
+	background(0)
+	if(mouseY>=height/2 && mouseX<=width/2) {
+	
+		fill(93,155,155); //azul
+	}	
+else if (mouseY<height/2 && mouseX<=width/2) {
+
+	fill (178,34,34); //vermelho
+}
+else if(mouseY<height/2 && mouseX>width/2) { 
+
+	fill(119,221,119); //verde
+} 
+else{
+	noFill();
+}
+
 	stroke(230);
 	strokeWeight(2);
-	noFill();
-	circle(width / 2, height / 2, height / 4);
+	circle(width / 2, height / 2, height / 2);
 
 
 }
